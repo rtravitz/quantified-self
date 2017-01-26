38c28b39aa2b3beee1c1c36cc75d996d7e6cc231
Ryan Travitz <rtravitz@gmail.com>
2017-01-25 22:22:58 -0700

    Change foods create test

--- test/foods_test.js
+++ test/foods_test.js
@@ -15,7 +15,7 @@ test.describe("foods.html", function() {
     driver.quit();
   });
 
-  test.it("user can create a new food with a name and calories", function() {
+  test.it("user can create a new food", function() {
     driver.get('http://localhost:8080/foods.html');
     var name = driver.findElement({name: 'name'});
     var calories = driver.findElement({name: 'calories'});
@@ -25,24 +25,14 @@ test.describe("foods.html", function() {
     calories.sendKeys('150');
     submit.click();
 
-    driver.findElement({id: 'food-table'}).then(function(table) {
-      table.findElements(webdriver.By.css('tr')).then(function(rows) {
-        assert.equal(rows.length, 2);
-
-        rows[1].findElement(webdriver.By.className('name-cell'))
-          .getText().then(function(foodName) {
-            assert.equal(foodName, 'apple');
-        });
-
-        rows[1].findElement(webdriver.By.className('calories-cell'))
-          .getText().then(function(foodCalories) {
-            assert.equal(foodCalories, '150');
-        });
-      });
+    driver.findElement({css: '.food-table tbody tr td:nth-of-type(1)'})
+    .getText().then(function(foodName) {
+      assert.equal(foodName, 'apple');
     });
 
-    driver.executeScript('return window.localStorage["foods"]').then(function(storedFoods) {
-      assert.equal(storedFoods, '[{"name":"apple","calories":"150"}]');
+    driver.findElement({css: '.food-table tbody tr td:nth-of-type(2)'})
+    .getText().then(function(foodCalories) {
+      assert.equal(foodCalories, '150');
     });
   });
 });
